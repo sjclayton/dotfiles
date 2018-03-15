fe952f1c0624a8d69ae7ded6c770bda7dc2c9428
Shaun J. Clayton <shaun.j.clayton@gmail.com>
2018-03-15 10:55:45 -0400

    Update polybar, i3, scripts

--- scripts/polylaunch.sh
+++ scripts/polylaunch.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/env sh
+#!/bin/bash
+
+MON0=`xrandr -q | grep 'HDMI1 connected'`
+MON1=`xrandr --output eDP1 --auto --output HDMI1 --primary --auto --right-of eDP1`
+MON2=`xrandr --output eDP1 --primary --auto`
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,10 +10,17 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar -r main &
-polybar -r extra &
+if [ -n "$MON0" ] || [ ! "$MON0" = "" ]; then 
+	echo $MON1
+	sleep 1
+	polybar -r main &
+	polybar -r extra &
+	echo "Bars launched..."
+else
+	echo $MON2
+	sleep 1
+	MONITOR=eDP1 polybar -r main &i
+	echo "Bar launched..."
+fi
 
-
-echo "Bars launched..."
-
+exit 0;
