4e6fddc3592ba9f919105b34b1659403be85b9ce
Shaun J. Clayton <shaun.j.clayton@gmail.com>
2018-03-15 09:16:29 -0400

    Update polybar, aliases, i3, scripts

--- scripts/spotify.py
+++ scripts/spotify.py
@@ -6,7 +6,7 @@ try:
 	spotify = bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
 	spotify_iface = dbus.Interface(spotify, 'org.freedesktop.DBus.Properties')
 	props = spotify_iface.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
-	print(str (' ' + props['xesam:artist'][0]) + " - " + str(props['xesam:title']))
+	print(str ('  ' + props['xesam:artist'][0]) + " - " + str(props['xesam:title']))
 	exit
 except dbus.exceptions.DBusException:
 	exit
