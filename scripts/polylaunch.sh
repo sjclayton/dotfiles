b2bf44b645944ba3e033ec7ea3e614de92cc6755
Shaun J. Clayton <shaun.j.clayton@gmail.com>
2018-04-01 02:18:52 -0400

    Updated i3, neofetch, polybar, ranger, zsh, scripts. Add xinit, zprofile.

--- scripts/polylaunch.sh
+++ scripts/polylaunch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 MON0=`xrandr -q | grep 'HDMI1 connected'`
-MON1=`xrandr --output eDP1 --auto --output HDMI1 --primary --auto --right-of eDP1`
-MON2=`xrandr --output eDP1 --primary --auto`
+MON1=`xrandr --output eDP1 --auto --output HDMI1 --auto --right-of eDP1`
+MON2=`xrandr --output eDP1 --auto`
 
 # Terminate already running bar instances
 killall -q polybar
@@ -12,12 +12,16 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if [ -n "$MON0" ] || [ ! "$MON0" = "" ]; then 
 	echo $MON1
+	#xrandr --dpi 92x88
+	xrandr --dpi 96
 	sleep 1
 	polybar -r main &
 	polybar -r docked &
 	echo "Bars launched..."
 else
 	echo $MON2
+	#xrandr --dpi 99x103
+	xrandr --dpi eDP1
 	sleep 1
 	polybar -r mobile &
 	echo "Bar launched..."
