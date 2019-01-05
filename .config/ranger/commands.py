d3659c0f013bc67a17379aef8e9511a5658a8057
Shaun J. Clayton <shaun.j.clayton@gmail.com>
2019-01-05 16:08:54 -0500

    Updated i3, Fixed ranger issues (image previews) and updated configs.

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -1,5 +1,18 @@
+# This is a sample commands.py.  You can add your own commands here.
+#
+# Please refer to commands_full.py for all the default commands and a complete
+# documentation.  Do NOT add them all here, or you may end up with defunct
+# commands when upgrading ranger.
+
+# A simple command for demonstration purposes follows.
+# -----------------------------------------------------------------------------
+
+from __future__ import (absolute_import, division, print_function)
+
+# You can import any python module as needed.
 import os
 
+# You always need to import ranger.api.commands here to get the Command class:
 from ranger.api.commands import Command
 
 class fzf_select(Command):
@@ -46,16 +59,3 @@ class fzf_locate(Command):
                 self.fm.cd(fzf_file)
             else:
                 self.fm.select_file(fzf_file)
-
-class fasd(Command):
-    """
-    :fasd
-
-    Jump to directory using fasd
-    """
-    def execute(self):
-        import subprocess
-        arg = self.rest(1)
-        if arg:
-            directory = subprocess.check_output(["fasd", "-d"]+arg.split(), universal_newlines=True).strip()
-            self.fm.cd(directory)                
