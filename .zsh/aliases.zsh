84e2cbad69b043b344f293d81a25683b39888edb
Shaun J. Clayton <shaun.j.clayton@gmail.com>
2019-02-06 19:15:32 -0500

    Update kitty, aliases. Remove sxiv.

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -12,7 +12,6 @@ alias sudo="nocorrect sudo -E "
 alias updateindex="updatedb -l 0 -o ~/.Archive.db -U /var/run/media/shaun/Archive && updatedb -l 0 -o ~/.home.db -U /home/shaun"
 
 # Shortcuts
-alias note="sncli"
 alias zshrc="$EDITOR ~/.zshrc"
 alias vimcfg="$EDITOR ~/.config/nvim/init.vim"
 alias aliases="$EDITOR ~/.zsh/aliases.zsh"
