61ad34d1813c597da4ae5cb891a583349df233f9
Shaun J. Clayton <shaun.j.clayton@gmail.com>
2019-03-09 04:09:46 -0500

    Add tablet script

---
+++ scripts/tablet.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Tablet (Express Keys)
+xsetwacom --set 21 Button 1 "key super"  # Key A
+xsetwacom --set 21 Button 2 "key shift"  # Key B
+xsetwacom --set 21 Button 3 "key ctrl z" # Key C
+xsetwacom --set 21 Button 8 "key ctrl"   # Key D
+
+# Stylus
+xsetwacom --set 22 Button 2 "key e"  # Lower Button
+
+# General
+
+# xsetwacom --set 22 PressureCurve 0 0 100 100 # Default (linear)
+xsetwacom --set 22 PressureCurve 0 25 75 100   # Softer
